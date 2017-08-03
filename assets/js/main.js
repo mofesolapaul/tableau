4ed90697b0ef3f62c1abb896650b96313f3c19e2
Banjo Mofesola Paul <mpdepaule1@gmail.com>
2017-08-03 10:50:42 +0100

    Yaaassssss!!! It works just like I pictured it. Optimizations next 😁   😁   😁

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(() => {
     // wrap tableaus in tableaus, and de-bleau them
     $('table.tableau').each((i, t) => {
         // props
-        let cellWidth
+        let cellWidth, minCellWidth = 240
 
         // clone the original table
         let tableau = $('<div></div>').addClass('tableau')
@@ -24,6 +24,9 @@ $(() => {
         let colCount = $('tr:first-of-type>th', thead).length
         let thtd = $('th,td', tableau)
 
+        // include the first column in the header
+        firstCol.push($('th:first-of-type', thead)[0])
+
         // allow for the fix-positioned header
         inner.css('marginTop', thead.height())
 
@@ -41,6 +44,9 @@ $(() => {
             let _left = $(this).scrollLeft()
             let _top = $(this).scrollTop()
             thead.css('left', -_left)
+            firstCol.css('left', _left)
+            if (_left != 0) firstCol.addClass('floating')
+            else firstCol.removeClass('floating')
         })
 
         // do the dimensioning
@@ -48,6 +54,7 @@ $(() => {
 
         function dimension() {
             cellWidth = tableau.width() / colCount
+            cellWidth = cellWidth < minCellWidth ? minCellWidth : cellWidth // normalize width
             thtd.css('width', cellWidth)
             tbody.css('paddingLeft', cellWidth)
         }
